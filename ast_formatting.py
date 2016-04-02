4392f8a5c5cbb7a99183107a61c8c4a4e9c6e703
Kristofor Maynard <kristofor.maynard@gmail.com>
2016-04-01 22:49:15 -0400

    small / meaningless ast_formatting changes

--- ast_formatting.py
+++ ast_formatting.py
@@ -47,7 +47,8 @@ def format_type(node, default="UnknownType"):
     """Get the type of a literal as a str
 
     Args:
-        node (ast.expr): An ast node of some literal
+        node (ast.expr): an ast node of some literal
+        default (str): for when we end up in the weeds
 
     Returns:
         str: the type of Node
@@ -73,16 +74,18 @@ def format_type(node, default="UnknownType"):
     return s
 
 
-# def _test():
-#     tree = ast.parse("def myfunc(a0, a1: 'ano', a2: (int, float, 'ano'), "
-#                      "a3: ['a', 'b'], a4: {'dx0': int}, a5: 0, a6: None, "
-#                      "a7: A_B): pass")
-#     args = tree.body[0].args.args
-#     ans = [arg.annotation for arg in args]
-#     print("=======")  # pylint: disable=superfluous-parens
-#     for i, ano in enumerate(ans):
-#         print(i, format_node(ano))
-#     print("-------")  # pylint: disable=superfluous-parens
-#     for i, ano in enumerate(ans):
-#         print(i, ano, format_type(ano))
-# _test()
+def _main():
+    tree = ast.parse("def myfunc(a0, a1: 'ano', a2: (int, float, 'ano'), "
+                     "a3: ['a', 'b'], a4: {'dx0': int}, a5: 0, a6: None, "
+                     "a7: A_B): pass")
+    args = tree.body[0].args.args
+    ans = [arg.annotation for arg in args]
+    print("=======")  # pylint: disable=superfluous-parens
+    for i, ano in enumerate(ans):
+        print(i, format_node(ano))
+    print("-------")  # pylint: disable=superfluous-parens
+    for i, ano in enumerate(ans):
+        print(i, ano, format_type(ano))
+
+if __name__ == "__main__":
+    _main()
